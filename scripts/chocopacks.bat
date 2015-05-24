28b87697980afabba53d54eebddd7cab60d19310
dnmgns <magnus@dnmgns.com>
2015-05-24 22:48:28 +0200

    fix choco elevated issue

--- scripts/chocopacks.bat
+++
@@ -1,7 +0,0 @@
-:: Ensure C:\Chocolatey\bin is on the path
-set /p PATH=<C:\Windows\Temp\PATH
-
-:: Install all the things; for example:
-cmd /c choco install notepadplusplus
-cmd /c choco install dotnet4.5.2
-cmd /c choco install vs2013.3
